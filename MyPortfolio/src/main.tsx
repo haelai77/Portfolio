c0fcdde41d4ad3ca06522b182613ae5f120ea4a4
Haelai77 <leojacklai@outlook.com>
2025-11-13 22:30:44 +0000

    fixing prod and local routing

--- MyPortfolio/src/main.tsx
+++ MyPortfolio/src/main.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './app/App.tsx'
 
+const isProd = import.meta.env.MODE === 'production';
+const basename = isProd ? '/Portfolio/' : '/';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
